9ae7cde57c08429fb68f41025d522e2d674e2b4a
sirachuchTT31 <sirachuch.kho@softpomarket.com>
2024-08-27 16:54:35 +0700

    fix : utils

--- index.ts
+++ index.ts
@@ -1,2 +1,8 @@
-export { CovertUTC0000 } from './utils/utils'
+// export { CovertUTC0000 } from './utils/utils'
+import { CovertUTC0000 } from "./utils/utils";
 
+declare namespace Utils {
+    var Convert: typeof CovertUTC0000;
+}
+
+export = Utils;
